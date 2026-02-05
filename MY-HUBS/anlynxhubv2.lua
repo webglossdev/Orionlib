494fb9339d281ce71b605291acb4203776815d86
webglossdev <112589206+webglossdev@users.noreply.github.com>
2026-02-05 03:06:45 -0300

    Update anlynxhubv2.lua

--- MY-HUBS/anlynxhubv2.lua
+++ MY-HUBS/anlynxhubv2.lua
@@ -3,53 +3,51 @@
 ]]
 -- aqui começa o script né chat
 
--- isso aqui checa se seu avatar e r6 ou r15 ou r34 ops, digitei errado, bora biu ne 
-loadstring(game:HttpGet("https://raw.githubusercontent.com/anlynxx/Orionlib/refs/heads/main/MY-HUBS/r6r15"))()
+-- isso aqui checa se seu avatar e r6 ou r15 
+loadstring(game:HttpGet("https://raw.githubusercontent.com/webglossdev/Orionlib/refs/heads/main/MY-HUBS/r6r15"))()
 
--- aqui carrega a disgraça do ui library fudido hosteado no meu proprio rep do github 
-local library = loadstring(game:HttpGet("https://raw.githubusercontent.com/anlynxx/Orionlib/refs/heads/main/apple"))()
+-- aqui carrega a ui library  
+local library = loadstring(game:HttpGet("https://raw.githubusercontent.com/webglossdev/Orionlib/refs/heads/main/apple"))()
 
--- aqui é a porra do titulo da GUI ne 
 local window = library:init("Anlynx Hub V2.0.2 (RShift to Hide)", true, Enum.KeyCode.RightShift, true)
 
--- O titulo da aba de Universais, ta lendo ainda pq porra
 window:Divider("Universal")
 
--- a aba de configuraçoes du pleier jhdfuhiahu
+-- a aba de configuraçoes
 local sectionA = window:Section("Player Settings")
 
--- vou parar de ficar comentando no meu proprio codigo, ta chato dms
+
 sectionA:TextField("Player Speed", "Enter Speed", function(Status)
-    getgenv().Enabled = true -- sabe o que que é isso? não sabe? fo-da-se, ta lendo pq quer porra
-getgenv().Speed = Status -- hmm o que sera que isso possivelmente pode fazer
-loadstring(game:HttpGet(('https://raw.githubusercontent.com/anlynxx/Orionlib/refs/heads/main/speed')))()
-end) -- MEU DEUS... UM LOADSTRING OH MY GOD, CHOCANTE
+    getgenv().Enabled = true 
+getgenv().Speed = Status 
+loadstring(game:HttpGet(('https://raw.githubusercontent.com/webglossdev/Orionlib/refs/heads/main/speed')))()
+end)
 
 sectionA:TextField("Player Max Zoom", "Enter Max Zoom", function(Status)
 game:GetService("Players").LocalPlayer.CameraMaxZoomDistance = Status
 end)
-sectionA:Button("Enable FullBright (Disables Shadows)", function() -- Fds vcs lendo, vou comentar nisso aqui n
-    loadstring(game:HttpGet(('https://raw.githubusercontent.com/anlynxx/Orionlib/refs/heads/main/FullBright')))()
+sectionA:Button("Enable FullBright (Disables Shadows)", function()
+    loadstring(game:HttpGet(('https://raw.githubusercontent.com/webglossdev/Orionlib/refs/heads/main/FullBright')))()
 end)
 
 local sectionA = window:Section("Universal Scripts")
 sectionA:Button("Infinite Yield", function() 
-    loadstring(game:HttpGet(('https://raw.githubusercontent.com/anlynxx/Orionlib/refs/heads/main/yield.lua')))()
+    loadstring(game:HttpGet(('https://raw.githubusercontent.com/webglossdev/Orionlib/refs/heads/main/yield.lua')))()
 end)
 sectionA:Button("Walk on Walls FE", function() 
-    loadstring(game:HttpGet(('https://raw.githubusercontent.com/anlynxx/Orionlib/refs/heads/main/walk-on-walls.lua')))()
+    loadstring(game:HttpGet(('https://raw.githubusercontent.com/webglossdev/Orionlib/refs/heads/main/walk-on-walls.lua')))()
 
 end)
 sectionA:Button("Hamster Ball FE", function() 
-    loadstring(game:HttpGet(('https://raw.githubusercontent.com/anlynxx/Orionlib/refs/heads/main/hamster-ball.lua')))()
+    loadstring(game:HttpGet(('https://raw.githubusercontent.com/webglossdev/Orionlib/refs/heads/main/hamster-ball.lua')))()
 
 end)
 sectionA:Button("Fling FE (Without spinning character)", function() 
-    loadstring(game:HttpGet(('https://raw.githubusercontent.com/anlynxx/Orionlib/refs/heads/main/Fling.lua')))()
+    loadstring(game:HttpGet(('https://raw.githubusercontent.com/webglossdev/Orionlib/refs/heads/main/Fling.lua')))()
 
 end)
 sectionA:Button("ESP / TRACERS", function() 
-    loadstring(game:HttpGet(('https://raw.githubusercontent.com/anlynxx/Orionlib/refs/heads/main/esp.lua')))()
+    loadstring(game:HttpGet(('https://raw.githubusercontent.com/webglossdev/Orionlib/refs/heads/main/esp.lua')))()
 
 end)
 sectionA:Button("Simple Spy V3", function() 
@@ -63,7 +61,7 @@ end)
 end)
 
 
---agora sim ein 
+--Lá ele
 local sectionB = window:Section("SUS ANIMATIONS R6")
 sectionB:Button("Bang V2", function() 
 loadstring(game:HttpGet(('https://pastebin.com/raw/aPSHMV6K')))()
@@ -103,20 +101,20 @@ end)
 
 window:Divider("Other Games")
 local sectionD = window:Section("Games")
-sectionD:Button("Dandy's World", function() -- Just kys lil bro
-loadstring(game:HttpGet(('https://raw.githubusercontent.com/anlynxx/Orionlib/refs/heads/main/dandy.lua')))()
+sectionD:Button("Dandy's World", function() 
+loadstring(game:HttpGet(('https://raw.githubusercontent.com/webglossdev/Orionlib/refs/heads/main/dandy.lua')))()
 end)
-sectionD:Button("Steal a Baddie", function() -- Just kys lil bro
-loadstring(game:HttpGet(('https://github.com/anlynxx/Orionlib/raw/refs/heads/main/Scripts/stealabaddie')))()
+sectionD:Button("Steal a Baddie", function() 
+loadstring(game:HttpGet(('https://github.com/webglossdev/Orionlib/raw/refs/heads/main/Scripts/stealabaddie')))()
 end)
 sectionD:Button("Natural Disaster Survival", function() 
-loadstring(game:HttpGet(('https://raw.githubusercontent.com/anlynxx/Orionlib/refs/heads/main/nds.lua')))()
+loadstring(game:HttpGet(('https://raw.githubusercontent.com/webglossdev/Orionlib/refs/heads/main/nds.lua')))()
 end)
-sectionD:Button("Prison Life Admin", function() -- O GOAT!
-loadstring(game:HttpGet(('https://raw.githubusercontent.com/anlynxx/Orionlib/refs/heads/main/prizzlife')))()
+sectionD:Button("Prison Life Admin", function()
+loadstring(game:HttpGet(('https://raw.githubusercontent.com/webglossdev/Orionlib/refs/heads/main/prizzlife')))()
 end)
-sectionD:Button("Prison Life Admin 2", function() -- DE NOVO!
-loadstring(game:HttpGet(('https://raw.githubusercontent.com/anlynxx/Orionlib/refs/heads/main/prisonlife2')))()
+sectionD:Button("Prison Life Admin 2", function()
+loadstring(game:HttpGet(('https://raw.githubusercontent.com/webglossdev/Orionlib/refs/heads/main/prisonlife2')))()
 end)
 sectionD:Button("MURDER MYSTERY 2", function() 
 loadstring(game:GetObjects("rbxassetid://4001118261")[1].Source)()
@@ -125,7 +123,7 @@ end)
 
 local sectionE = window:Section("Fling Things and People")
 sectionE:Button("Extra Features", function() 
-loadstring(game:HttpGet(('https://raw.githubusercontent.com/anlynxx/Orionlib/refs/heads/main/FTAP2.lua')))()
+loadstring(game:HttpGet(('https://raw.githubusercontent.com/webglossdev/Orionlib/refs/heads/main/FTAP2.lua')))()
 end)
 sectionE:TextField("Flinging Strengh", "Enter Fling Force", function(Status)
 local bodyvel_Name = "FlingVelocity"
@@ -134,7 +132,7 @@ local w = game:GetService("Workspace")
 local r = game:GetService("RunService")
 local d = game:GetService("Debris")
 local strength = Status
-loadstring(game:HttpGet(('https://raw.githubusercontent.com/anlynxx/Orionlib/refs/heads/main/FTAP.lua')))()
+loadstring(game:HttpGet(('https://raw.githubusercontent.com/webglossdev/Orionlib/refs/heads/main/FTAP.lua')))()
 end)
 
 
@@ -142,5 +140,5 @@ local sectionF = window:Section("Reanimation Script")
 sectionF:Label("REANIMATION ONLY WORKS IN MIC UP OR")
 sectionF:Label("MEET PEOPLE ACROSS THE WORLD!")
 sectionF:Button("Execute Stalkie (Reanimation)", function()
-loadstring(game:HttpGet(('https://raw.githubusercontent.com/anlynxx/Orionlib/refs/heads/main/Reanimation')))()
+loadstring(game:HttpGet(('https://raw.githubusercontent.com/webglossdev/Orionlib/refs/heads/main/Reanimation')))()
 end)
